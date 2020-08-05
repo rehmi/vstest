62039fbd20490a349e89443679aaedc886c4ace3
Rehmi Post <rehmi.post@samsung.com>
2020-08-05 16:41:13 -0700

    initial version

---
+++ main.jl
@@ -0,0 +1,14 @@
+using VegaLite, DataFrames, Query, VegaDatasets
+
+cars = dataset("cars")
+
+cars |> @select(:Acceleration, :Name) |> collect
+
+function foo(data, origin)
+    df = data |> @filter(_.Origin == origin)
+    return df |> @vlplot(:point, :Acceleration, :Miles_per_Gallon)
+end
+
+p = foo(cars, "USA")
+
+p |> save("foo.png")
